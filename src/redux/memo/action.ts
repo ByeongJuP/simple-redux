68ee17328eafce7e33fec2bac2bcca0c99c3192c
ByeongJuP <pbju92@gmail.com>
2022-07-09 12:48:56 +0900

    memo component with redux

---
+++ src/redux/memo/action.ts
@@ -0,0 +1,2 @@
+export const MEMO_CREAET = 'MEMO_CREATE';
+export const MEMOO_DELETE = 'MEMO_DELETE';
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import counterReducer from './count/counter';
-const rootReducer = combineReducers({ counterReducer });
+import memoReducer from './memo/memo';
+
+const rootReducer = combineReducers({ counterReducer, memoReducer });
 
 export default rootReducer;
 export type RootState = ReturnType<typeof rootReducer>;
